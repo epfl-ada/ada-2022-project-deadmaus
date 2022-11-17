8572d8b212ad54d66941b7fb82ecd059e7d27d3c
amey-kulkarni27 <amey2704@gmail.com>
2022-11-17 19:35:01 +0100

    script to create links from current wikipedia

--- Wiki_2022/scrape.py
+++ Wiki_2022/scrape.py
@@ -2,6 +2,7 @@ import pandas as pd
 from urllib.parse import quote, unquote
 from bs4 import BeautifulSoup
 import requests
+from tqdm import tqdm
 
 
 def get_titles():
@@ -9,7 +10,6 @@ def get_titles():
     Return pandas dataframe consisting of the titles of the articles present in the dataset
     '''
     articles = pd.read_csv('wikispeedia_paths-and-graph/articles.tsv', comment='#', delimiter='\t', encoding='utf8', names=['article']) 
-    articles['article'] = articles['article'].apply(unquote)
     return articles
 
 def title_from_link(link):
@@ -36,22 +36,31 @@ def get_links(soup, articles):
 
         
         if title in articles.article.values:
-            links.append(link)
+            links.append(title)
 
     # Remove repitions
     links = list(set(links))
-    print(len(links))
+    # print(len(links))
 
     return links
 
 if __name__ == "__main__":
     articles = get_titles()
     base_url = "https://en.wikipedia.org/wiki/"
-    for title in articles["article"].sample(5):
+      
+    res_df = pd.DataFrame()
+    all_links = []
+    for qtitle in tqdm(articles["article"]):
+        title = unquote(qtitle)
         url = base_url + title
-        print(url)
+        # print(url)
         page = requests.get(url)
         soup = BeautifulSoup(page.content, "html.parser")
         outgoing_links = get_links(soup, articles)
-        
+        for ol in outgoing_links:
+            all_links.append((qtitle, ol))
+        # print(all_links)
+    links = pd.read_csv('wikispeedia_paths-and-graph/links.tsv', comment='#', delimiter='\t', names=['linkSource', 'linkTarget'])
+    links22 = pd.DataFrame(all_links, columns=['linkSource', 'linkTarget'])
+    links.to_csv("links22.tsv", sep='\t', index=False)
 
