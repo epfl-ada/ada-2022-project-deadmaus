2ca9b4d2296289ad659b0700732536e6b071c521
amey-kulkarni27 <amey2704@gmail.com>
2022-12-17 16:34:25 +0100

    scraping for all years

---
+++ Wiki_2022/scrape_years.py
@@ -0,0 +1,57 @@
+import pandas as pd
+from urllib.parse import quote, unquote
+import pywikibot
+from itertools import islice
+from time import time
+from bs4 import BeautifulSoup
+import requests
+from tqdm import tqdm
+from  scrape import get_links, get_titles, title_from_link
+
+
+
+if __name__ == "__main__":
+    st = time()
+    articles = get_titles()
+    # print(articles.head(2))
+    ctr = 0
+    site = pywikibot.Site("en", "wikipedia")
+    articles_set = set(articles.article.values)
+    pg = pywikibot.Page(site, "%C3%81ed%C3%A1n_mac_Gabr%C3%A1in")
+    x = (pg.revisions(content=True, total=1, starttime="2011-01-01T00:00:00Z", endtime="2011-12-31T00:00:00Z", reverse=True))
+    # print(next(x).revid)
+    for article in articles["article"].head(1):
+        title = (unquote(article))
+        # print(title, article)
+        base_url = "https://en.wikipedia.org/w/index.php?"
+        url = base_url + title
+        page = pywikibot.Page(site, article)
+        prev_arts = page.revisions(content=True, total=1, starttime="2009-12-31T00:00:00Z", reverse=False)
+        assert(prev_arts != None)
+        prev_art = next(prev_arts)
+        # print(prev_art.timestamp)
+        for year in range(2010, 2022):
+            page = pywikibot.Page(site, article)
+            try:
+                revs = page.revisions(content=True, total=1, starttime=str(year)+"-01-01T00:00:00Z", endtime=str(year)+"-12-31T00:00:00Z", reverse=True)
+            except:
+                rev = prev_art
+            else:
+                prev = revs
+                rev = next(revs)
+                # print(rev.timestamp)
+            # print(rev.revid)
+            old_id = rev.revid
+            urls = url + "&oldid=" + str(old_id)
+            # print(urls)
+            page = requests.get(urls)
+            soup = BeautifulSoup(page.content, "html.parser")
+            outgoing_links = get_links(soup, articles_set)
+            all_links = []
+            for ol in outgoing_links:
+                all_links.append((article, ol))
+            # print(len(all_links))
+            # print(all_links)
+    ctr += 1
+    en = time()
+    print(en-st)
