ddc03948438365047502f1f46a9e76f9ac2b090e
amey-kulkarni27 <amey2704@gmail.com>
2022-12-18 13:46:28 +0100

    fixed position of links list

--- Wiki_2022/scrape_years.py
+++ Wiki_2022/scrape_years.py
@@ -15,41 +15,41 @@ if __name__ == "__main__":
     st = time()
     articles = get_titles()
     # print(articles.head(2))
-    site = pywikibot.Site("en", "wikipedia")
     articles_set = set(articles.article.values)
-    pg = pywikibot.Page(site, "%C3%81ed%C3%A1n_mac_Gabr%C3%A1in")
-    x = (pg.revisions(content=True, total=1, starttime="2011-01-01T00:00:00Z", endtime="2011-12-31T00:00:00Z", reverse=True))
+    site = pywikibot.Site("en", "wikipedia")
+    links09 = pd.read_csv('wikispeedia_paths-and-graph/links.tsv', comment='#', delimiter='\t', names=['linkSource', 'linkTarget'])
     # print(next(x).revid)
     for article in tqdm(articles["article"]):
-        cur_links = [] # list of tuples (source, target) for current year and current article
+        prev_links = list(links09[links09["linkSource"] == article].itertuples(index=False, name=None)) # link structure from the wikispeedia dataset
         title = (unquote(article))
-        # print(title, article)
         base_url = "https://en.wikipedia.org/w/index.php?"
         url = base_url + title
         page = pywikibot.Page(site, article)
-        prev_arts = page.revisions(content=True, total=1, starttime="2009-12-31T00:00:00Z", reverse=False)
-        assert(prev_arts != None)
-        prev_art = next(prev_arts)
-        # print(prev_art.timestamp)
+        num = 0
         for year in range(2010, 2022):
+            cur_links = [] # list of tuples (source, target) for current year and current article
             page = pywikibot.Page(site, article)
             try:
                 revs = page.revisions(content=True, total=1, starttime=str(year)+"-01-01T00:00:00Z", endtime=str(year)+"-12-31T00:00:00Z", reverse=True)
-            except:
-                rev = prev_art
-            else:
-                prev = revs
                 rev = next(revs)
-            old_id = rev.revid
-            urls = url + "&oldid=" + str(old_id)
-            page = requests.get(urls)
-            soup = BeautifulSoup(page.content, "html.parser")
-            outgoing_links = get_links(soup, articles_set) # order of links seems to be completely random
-            for ol in outgoing_links:
-                cur_links.append((article, ol))
+            except:
+                cur_links = prev_links
+            else:
+                num += 1
+                prev = revs
+                old_id = rev.revid
+                urls = url + "&oldid=" + str(old_id)
+                page = requests.get(urls)
+                soup = BeautifulSoup(page.content, "html.parser")
+                outgoing_links = get_links(soup, articles_set) # order of links seems to be completely random
+                for ol in outgoing_links:
+                    cur_links.append((article, ol))
+                prev_links = cur_links
             # print(len(cur_links))
             links = pd.DataFrame(cur_links, columns=['linkSource', 'linkTarget'])
             output_path = "Wiki_Revs/links"+str(year)[-2:]+".tsv"
             links.to_csv(output_path, sep="\t", index=False, mode='a', header=not os.path.exists(output_path))
+        if num < 12:
+            print(num)
     en = time()
     print(en-st)
