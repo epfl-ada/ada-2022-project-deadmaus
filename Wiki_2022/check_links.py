44ba3890fb84a369ebc9d3b7ba73d673988d8b6d
amey-kulkarni27 <amey2704@gmail.com>
2022-12-19 02:16:46 +0100

    check if outgoing links exist for given

---
+++ Wiki_2022/check_links.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from urllib.parse import quote, unquote
+import pywikibot
+from itertools import islice
+from time import time
+from bs4 import BeautifulSoup
+import requests
+from  scrape import get_links, get_titles
+from urllib.parse import quote, unquote
+
+site = pywikibot.Site("en", "wikipedia")
+title = unquote("DNA_repair")
+page = pywikibot.Page(site, title)
+year = 2021
+revs = page.revisions(content=True, total=1, starttime=str(year)+"-01-01T00:00:00Z", endtime=str(year)+"-12-31T00:00:00Z", reverse=True)
+rev = next(revs)
+print(rev.timestamp)
+old_id = rev.revid
+base_url = "https://en.wikipedia.org/w/index.php?"
+url = base_url + title
+urls = url + "&oldid=" + str(old_id)
+print(urls)
+page = requests.get(urls)
+soup = BeautifulSoup(page.content, "html.parser")
+articles = get_titles()
+articles_set = set(articles.article.values)
+outgoing_links = get_links(soup, articles_set) # order of links seems to be completely random
+print(len(outgoing_links))
